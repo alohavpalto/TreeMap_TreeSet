388db457ee0e6ec16c80a5edc949722caaa2df9e
OtlapPalto <marby1palto@gmail.com>
2023-01-01 18:06:08 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    List<Person> persons = new ArrayList<>();
+    persons.add(new Person("Anna", "Chernova Sanior", 30));
+    persons.add(new Person("Dori", "Rybkin's youngest daughter ", 10));
+    persons.add(new Person("Dima", "Gulyaev", 25));
+    persons.add(new Person("Tanya", "By her own", 40));
+    System.out.println(persons);
+    Collections.sort(persons, new PersonComparator(1));
+    System.out.println(persons);
+  }
+}
---
+++ src/PersonComparator.java
@@ -0,0 +1,21 @@
+import java.util.Comparator;
+
+public class PersonComparator implements Comparator<Person> {
+  private int maxCountWords;
+
+  public PersonComparator(int maxCountWords) {
+    this.maxCountWords = maxCountWords;
+  }
+
+  @Override
+  public int compare(Person p1, Person p2) {
+    if (p1.getWordsSurname() < maxCountWords || p2.getWordsSurname() < maxCountWords) {
+      if (p1.getWordsSurname() < p2.getWordsSurname()) {
+        return 1;
+      } else if (p1.getWordsSurname() > p2.getWordsSurname()) {
+        return -1;
+      }
+    }
+    return Integer.compare(p2.getAge(), p1.getAge());
+  }
+}
