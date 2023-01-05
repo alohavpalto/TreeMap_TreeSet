12fed91b60249bf3cb50dd17ec14047766b1ba1c
OtlapPalto <marby1palto@gmail.com>
2023-01-05 21:28:02 +0300

    revision

--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,10 @@ public class Main {
     persons.add(new Person("Dori", "Rybkin's youngest daughter ", 10));
     persons.add(new Person("Dima", "Gulyaev", 25));
     persons.add(new Person("Tanya", "By her own", 40));
+    persons.add(new Person("Kseha", "Through-Dash", 40));
+    persons.add(new Person("Hino", "Head of-the family", 40));
     System.out.println(persons);
-    Collections.sort(persons, new PersonComparator(1));
+    Collections.sort(persons, new PersonComparator(2));
     System.out.println(persons);
   }
 }
