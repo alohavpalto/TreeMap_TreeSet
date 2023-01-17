a2ab93b0e0858c1c2d4252d3c8ae6982270fff49
OtlapPalto <marby1palto@gmail.com>
2023-01-17 21:49:53 +0300

    case "-"

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,10 @@ public class Main {
 
     List<Person> persons = new ArrayList<>();
     persons.add(new Person("Anna", "Chernova Sanior", 30));
-    persons.add(new Person("Dori", "Rybkin's youngest daughter ", 10));
     persons.add(new Person("Dima", "Gulyaev", 25));
     persons.add(new Person("Tanya", "By her own", 40));
-    persons.add(new Person("Kseha", "Through-Dash", 40));
-    persons.add(new Person("Hino", "Head of-the family", 40));
+    persons.add(new Person("Kseha", "Through-Dash",20 ));
+
     System.out.println(persons);
     Collections.sort(persons, new PersonComparator(2));
     System.out.println(persons);
--- src/PersonComparator.java
+++ src/PersonComparator.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 
 public class PersonComparator implements Comparator<Person> {
+
   private int maxCountWords;
 
   public PersonComparator(int maxCountWords) {
@@ -9,6 +12,11 @@ public class PersonComparator implements Comparator<Person> {
 
   @Override
   public int compare(Person p1, Person p2) {
+    List<String> surname1 = new ArrayList<>();
+    surname1.addAll(List.of(p1.getSurname().split("\\P{IsAlphabetic}+")));
+    List<String> surname2 = new ArrayList<>();
+    surname2.addAll(List.of(p2.getSurname().split("\\P{IsAlphabetic}+")));
+
     if (p1.getWordsSurname() < maxCountWords || p2.getWordsSurname() < maxCountWords) {
       if (p1.getWordsSurname() < p2.getWordsSurname()) {
         return 1;
