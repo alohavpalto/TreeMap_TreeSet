3eb097d281b3803cb830be1225faf53581674b87
OtlapPalto <marby1palto@gmail.com>
2023-02-08 22:24:41 +0300

    branch 'lambda'

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -8,12 +9,22 @@ public class Main {
 
     List<Person> persons = new ArrayList<>();
     persons.add(new Person("Anna", "Chernova", 50));
-    persons.add(new Person("Dima", "Gulyaev",40 ));
+    persons.add(new Person("Dima", "Gulyaev", 40));
     persons.add(new Person("Tanya", "By-her", 20));
-    persons.add(new Person("Kseha", "Through Dash",30 ));
-
+    persons.add(new Person("Kseha", "Through Dash", 30));
     System.out.println(persons);
-    Collections.sort(persons, new PersonComparator(2));
+
+    Comparator<Person> comparator = (p1, p2) -> {
+      if (p1.getWordsSurname() < 3 || p2.getWordsSurname() < 3) {
+        if (p1.getWordsSurname() < p2.getWordsSurname()) {
+          return 1;
+        } else if (p1.getWordsSurname() > p2.getWordsSurname()) {
+          return -1;
+        }
+      }
+      return Integer.compare(p2.getAge(), p1.getAge());
+    };
+    Collections.sort(persons, comparator);
     System.out.println(persons);
   }
 }
